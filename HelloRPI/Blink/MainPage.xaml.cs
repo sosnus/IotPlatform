7098e75d4dfd5a816e34e898d8b97d0d2462a964
sosnus <sosnus15@gmail.com>
2017-03-06 00:09:47 +0100

    Created and wrote Blink project

--- HelloRPI/Blink/MainPage.xaml.cs
+++ HelloRPI/Blink/MainPage.xaml.cs
@@ -1,23 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using System;
 using Windows.Devices.Gpio;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Media;
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -26,7 +9,7 @@ namespace Blink
 {
     public sealed partial class MainPage : Page
     {
-        private const int numer_pinu = 2;
+        private const int numer_pinu = 2; //number of pin where we plug led
         private GpioPin led; //class with led pin
 
         public MainPage()
