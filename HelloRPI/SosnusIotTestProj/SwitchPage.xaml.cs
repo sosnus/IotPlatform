875318bf050aada45719bb28da4ae62a28e852ec
sosnus <sosnus15@gmail.com>
2017-04-06 15:43:40 +0200

    New Pages for App

--- HelloRPI/SosnusIotTestProj/SwitchPage.xaml.cs
+++ HelloRPI/SosnusIotTestProj/SwitchPage.xaml.cs
@@ -13,29 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace SosnusIotTestProj
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class SwitchPage : Page
     {
-        public MainPage()
+        public SwitchPage()
         {
             this.InitializeComponent();
-            spMainStack1.Children.Add(new Info());
-            spMainStack1.Children.Add(new ServoPage());
-            spMainStack1.Children.Add(new LedGreenAndYellow());
-            spMainStack1.Children.Add(new LedRedPwm());
-            spMainStack2.Children.Add(new InterruptPage());
-            spMainStack2.Children.Add(new BuzzerPage());
-            spMainStack2.Children.Add(new SwitchPage());
         }
-
-
-
-
     }
 }
