01e72eb71bc4ca0dae6d0a8b6a6c8aad5e46a414
sosnus <sosnus15@gmail.com>
2017-04-09 15:16:35 +0200

    Some changes in Buzz.cs, InterruptPage updated

--- HelloRPI/SosnusIotLib/Pwm/Buzz.cs
+++ HelloRPI/SosnusIotLib/Pwm/Buzz.cs
@@ -8,8 +8,29 @@ namespace SosnusIotLib.Pwm
 {
     class Buzz : PwmBasic
     {
-      //  PwmServo buzz = new PwmServo();
+        //  PwmServo buzz = new PwmServo();
+        Buzz buzz = new Buzz();
+        public Buzz() { }
 
+        public async void SetupBuzz(int _pinNumber)
+        {
+            await SetupBasic(_pinNumber, 100); //this is temp frequency
+            State = false;
+        }
+
+
+        /// <summary>
+        /// For generate one tone
+        /// </summary>
+        /// <param name="time">time of this signal (in ms)</param>
+        /// <param name="frequency">frequency of this signal (in Hz)</param>
+        public void Tone(double time, double frequency)
+        {
+            //pwm enable
+            //set frequency
+            //set timer
+            //add pwm.stop to queue
+        }
 
     }
 }
--- HelloRPI/SosnusIotTestProj/InterruptPage.xaml.cs
+++ HelloRPI/SosnusIotTestProj/InterruptPage.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Gpio;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +11,50 @@ namespace SosnusIotTestProj
     /// </summary>
     public sealed partial class InterruptPage : Page
     {
+        private const int numer_pinu = 23; //number of pin where we plug switch
+                                           //one switch pin is still connected to ground
+        private GpioPin mySwitch;
+
         public InterruptPage()
         {
             this.InitializeComponent();
+            InitGPIO(); //Initialize all input and output pins
+        }
+
+
+        private void InitGPIO() //there we will write all procedures connected with pins initializations
+        {
+            var gpio = GpioController.GetDefault();
+            mySwitch = gpio.OpenPin(numer_pinu); //initialization
+            mySwitch.SetDriveMode(GpioPinDriveMode.InputPullUp); //Set directory (input/output)
+            mySwitch.ValueChanged += MySwitch_ValueChanged;
+        }
+
+        private void MySwitch_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            var t = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (args.Edge == GpioPinEdge.FallingEdge)
+                {
+                    tblInfo.Text = $"LOW state on pin {numer_pinu}";
+                }
+                else
+                {
+                    tblInfo.Text = $"HIGH state on pin {numer_pinu}";
+                }
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (mySwitch.Read() == GpioPinValue.Low)
+            {
+                tblInfo.Text = $"LOW state on pin {numer_pinu}";
+            }
+            else
+            {
+                tblInfo.Text = $"HIGH state on pin {numer_pinu}";
+            }
         }
     }
 }
