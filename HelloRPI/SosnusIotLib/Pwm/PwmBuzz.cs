7b3c165c35b7fe3cd9c360554e6650f6b74e11d1
sosnus <sosnus15@gmail.com>
2017-04-23 01:21:31 +0200

    start building PwmBuzz class

--- HelloRPI/SosnusIotLib/Pwm/PwmBuzz.cs
+++ HelloRPI/SosnusIotLib/Pwm/PwmBuzz.cs
@@ -8,7 +8,66 @@ namespace SosnusIotLib.Pwm
 {
     class PwmBuzz : PwmBasic
     {
+        private List<BuzzerEffectProperties> list = new List<BuzzerEffectProperties>();
+
         //Deklaracja listy tasków dla tego obiektu-pwmBuzz
+        //struct or class? class becouse in struct I must define ALL parametrs
+        public class BuzzerEffectProperties
+        {
+            BuzzerEffectProperties(double _frequency, double _signalDuration)
+            {
+                frequency = _frequency;
+                signalDuration = _signalDuration;
+                duty = 50;
+            }
+            public double duty;
+            double frequency;
+            double signalDuration;
+        }
+
+        public void addEffect(BuzzerEffectProperties tempBuzzerEffectProperties)
+        {
+            //BuzzerEffectPropertiKCes tempStruct;
+            //tempStruct.duty
+            list.Add(tempBuzzerEffectProperties);
+        }
+
+
+        /// <summary>
+        /// get firs element from List<BuzzerEffectProperties> and set pwm properties on device
+        /// </summary>
+        public void SetEffect()
+        {
+            //thread sleep();
+            BuzzerWorking = true;
+        }
+
+        private bool buzzerWorking = false;
+
+        public bool BuzzerWorking
+        {
+            get
+            {
+                return buzzerWorking;
+            }
+            set
+            {
+                if(list.Count!=0) //when object in list is waiting
+                {
+                    this.State = true; //startPwmPin
+                    SetEffect();
+                    buzzerWorking = true;
+                }
+                else //if list is empty set work on false
+                {
+                    this.State = false; //stopPwmPin
+                    buzzerWorking = false;
+                }
+
+                //ustaw na false ale sprawdź jeszcze raz kolejkę
+            }
+        }
+
 
 
         public PwmBuzz() { }
@@ -28,11 +87,11 @@ namespace SosnusIotLib.Pwm
         public void Tone(double time, double frequency)
         {
             //add to queue this functions:
-            //pwm enable
+            //pwm enable (working setter true)
             //set frequency
             //set timer
             //wait (or sleep?)
-            //pwm disable
+            //pwm disable (working setter false)
         }
 
     }
