508e84715980603c2200265f86e0b016bad7db73
sosnus <sosnus15@gmail.com>
2017-04-23 02:25:59 +0200

    PwmBuzzer LibClass update2

--- HelloRPI/SosnusIotLib/Pwm/PwmBuzz.cs
+++ HelloRPI/SosnusIotLib/Pwm/PwmBuzz.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,28 +20,36 @@ namespace SosnusIotLib.Pwm
             {
                 frequency = _frequency;
                 signalDuration = _signalDuration;
-                duty = 50;
+                fill = 50;
             }
-            public double duty;
-            double frequency;
-            double signalDuration;
+            public double fill;
+            public double frequency;
+            public double signalDuration;
         }
 
         public void addEffect(BuzzerEffectProperties tempBuzzerEffectProperties)
         {
-            //BuzzerEffectPropertiKCes tempStruct;
-            //tempStruct.duty
             list.Add(tempBuzzerEffectProperties);
+            BuzzerWorking = true;
         }
 
 
         /// <summary>
         /// get firs element from List<BuzzerEffectProperties> and set pwm properties on device
         /// </summary>
-        public void SetEffect()
+        public async void SetEffect()
         {
-            //thread sleep();
-            BuzzerWorking = true;
+            if(list.Count !=0)
+            {
+
+                //var a = list.First().duty;
+                //this.
+                Frequency = list.First().frequency;
+                Fill = list.First().fill;
+                await Task.Delay(TimeSpan.FromMilliseconds(list.First().signalDuration));
+                list.Remove(list.First());
+                BuzzerWorking = true;
+            }
         }
 
         private bool buzzerWorking = false;
@@ -52,16 +62,27 @@ namespace SosnusIotLib.Pwm
             }
             set
             {
-                if(list.Count!=0) //when object in list is waiting
+                if(value) //if setter is true
                 {
-                    this.State = true; //startPwmPin
-                    SetEffect();
-                    buzzerWorking = true;
+                    if(list.Count!=0) //when object in list is waiting
+                    {
+                        this.State = true; //startPwmPin
+                        SetEffect();
+                        buzzerWorking = true;
+                    }
+                    else //if list is empty set work on false
+                    {
+                        this.State = false; //stopPwmPin
+                        buzzerWorking = false;
+                    }
                 }
-                else //if list is empty set work on false
+                else
                 {
-                    this.State = false; //stopPwmPin
-                    buzzerWorking = false;
+                    Debug.WriteLine("It isn't possible!");
+                    Debug.WriteLine("Problem with BuzzerWorking{get;set;}!");
+                    //throw;
+                    //if (list.Count != 0) ; //when object in list is waiting
+
                 }
 
                 //ustaw na false ale sprawdź jeszcze raz kolejkę
@@ -72,27 +93,27 @@ namespace SosnusIotLib.Pwm
 
         public PwmBuzz() { }
 
-        public async void SetupBuzz(int _pinNumber)
-        {
-            await SetupBasic(_pinNumber, 100); //this is temp frequency
-            State = false;
-        }
+        //public async void SetupBuzz(int _pinNumber)
+        //{
+        //    await SetupBasic(_pinNumber, 100); //this is temp frequency
+        //    State = false;
+        //}
 
 
-        /// <summary>
-        /// For generate one tone
-        /// </summary>
-        /// <param name="time">time of this signal (in ms)</param>
-        /// <param name="frequency">frequency of this signal (in Hz)</param>
-        public void Tone(double time, double frequency)
-        {
-            //add to queue this functions:
-            //pwm enable (working setter true)
-            //set frequency
-            //set timer
-            //wait (or sleep?)
-            //pwm disable (working setter false)
-        }
+        ///// <summary>
+        ///// For generate one tone
+        ///// </summary>
+        ///// <param name="time">time of this signal (in ms)</param>
+        ///// <param name="frequency">frequency of this signal (in Hz)</param>
+        //public void Tone(double time, double frequency)
+        //{
+        //    //add to queue this functions:
+        //    //pwm enable (working setter true)
+        //    //set frequency
+        //    //set timer
+        //    //wait (or sleep?)
+        //    //pwm disable (working setter false)
+        //}
 
     }
 }
