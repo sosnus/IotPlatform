e0ed0eea9880b7346a113be3a10609808f7f1769
sosnus <sosnus15@gmail.com>
2017-04-23 03:25:48 +0200

    Update PwmBuzzerSimpleTest

--- HelloRPI/PwmBuzzerSimpleTest/MainPage.xaml.cs
+++ HelloRPI/PwmBuzzerSimpleTest/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using SosnusIotLib.Pwm;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +24,34 @@ namespace PwmBuzzerSimpleTest
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        PwmBasic Buzzer = new PwmBasic();
+
         public MainPage()
         {
             this.InitializeComponent();
+            InitGPIO(); //Initialize all input and output pins
+        }
+
+
+
+        private async void InitGPIO()
+        {
+            await Buzzer.SetupBasic(27, 100);
+        }
+
+
+
+        private void SetParam_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Buzzer.Frequency = sFrequency.Value*1000;
+            tblFrequency.Text = $"{Buzzer.Frequency.ToString()} Hz";
+        }
+
+        private void EnablePwm_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Buzzer.State = !Buzzer.State;
+            tblPwmState.Text = Convert.ToString($"Pwm is {Buzzer.State}");
         }
     }
 }
