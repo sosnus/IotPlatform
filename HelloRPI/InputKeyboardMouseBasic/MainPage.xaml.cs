d9e6ce1ea41cec1a9f0aad18b8602f91dfa25c87
sosnus <sosnus15@gmail.com>
2017-05-04 02:43:35 +0200

    Added and done Basic input

--- HelloRPI/InputKeyboardMouseBasic/MainPage.xaml.cs
+++ HelloRPI/InputKeyboardMouseBasic/MainPage.xaml.cs
@@ -12,6 +12,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+using System.Windows.Input;
+using Windows.UI.Core;
+using Windows.System;
+using System.Diagnostics;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +31,29 @@ namespace InputKeyboardMouseBasic
         public MainPage()
         {
             this.InitializeComponent();
+            Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated += AcceleratorKeyActivated;
+            GridMain.PointerMoved += new PointerEventHandler(Pointer_Moved);
         }
+
+        private void AcceleratorKeyActivated(CoreDispatcher sender, AcceleratorKeyEventArgs args)
+        {
+            if (args.EventType.ToString().Contains("Down"))
+            {
+                spKeyboard.Children.Insert(0, (new TextBlock()
+                {
+                    Text = Convert.ToString($"Pressed {args.VirtualKey} at " + DateTime.Now.ToString("h:mm:ss fffffff"))
+                }));
+            }
+        }
+
+        void Pointer_Moved(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Input.PointerPoint currentPoint = e.GetCurrentPoint(GridMain);
+            spMouse.Children.Insert(0, (new TextBlock()
+            {
+                Text = Convert.ToString($"Cursor on X={Math.Round(currentPoint.Position.X, 0)} Y={Math.Round(currentPoint.Position.Y, 0)} at " + DateTime.Now.ToString("h:mm:ss fffffff"))
+            }));
+        }
+
     }
 }
