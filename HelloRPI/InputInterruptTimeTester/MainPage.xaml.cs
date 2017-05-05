4153666e3c356559ff68b2129afe2c3691180b62
sosnus <sosnus15@gmail.com>
2017-05-05 22:39:44 +0200

    end  unnecessary lines

--- HelloRPI/InputInterruptTimeTester/MainPage.xaml.cs
+++ HelloRPI/InputInterruptTimeTester/MainPage.xaml.cs
@@ -1,31 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.Devices.Gpio;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.Devices.Gpio;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace InputInterruptTimeTester
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private GpioPin tactSwitch;
         private GpioPin led;
-
 
         public MainPage()
         {
@@ -42,7 +23,6 @@ namespace InputInterruptTimeTester
 
             var gpioLed = GpioController.GetDefault();
             led = gpioLed.OpenPin(5);
-            led.Write(GpioPinValue.High); 
             led.SetDriveMode(GpioPinDriveMode.Output);
         }
 
@@ -53,9 +33,8 @@ namespace InputInterruptTimeTester
                 else                                      led.Write(GpioPinValue.High);
         }
 
-
-        //advanced scenario
         /*
+        //advanced scenario
         int stateChangeCnt = 0;
         private void MySwitch_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
         {
@@ -79,8 +58,8 @@ namespace InputInterruptTimeTester
         }
         */
 
-        //simple scenario (but ready to change UI)
         /*
+        //simple scenario (but ready to change UI)
         private void MySwitch_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
         {
             var t = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
