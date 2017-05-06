3b302ede28f8d90c4d2e1c4d4eca306b7e74b734
sosnus <sosnus15@gmail.com>
2017-05-06 16:23:42 +0200

    algorithm maybe ok

--- SosnusIotPlatform/SosnusIotLib/Pwm/PwmServo360.cs
+++ SosnusIotPlatform/SosnusIotLib/Pwm/PwmServo360.cs
@@ -32,25 +32,32 @@
         /// <param name="wheelFill">between {-100;100 }</param>
         public void SetServo360(double wheelFill)
         {
-            //between {-100;100 } to between {4;11,5 }
-            Fill = percentPowerToFill(wheelFill);
+            //input: <-100;100>
+            //output: <4;11,5>
+            double tempFill = wheelFill;
+
+            tempFill = tempFill / 2 + 50;
+            tempFill = (tempFill * 7.5) / 100; // 7.5 mean fillDelta in percent
+            tempFill += 4;
+            Fill = tempFill;
         }
 
-        private double percentPowerToFill(double percent)
-        {
-            // -100% mean 0.8ms mean 4%
-            //    0% mean 1.5ms
-            //  100% mean 2.3ms mean 11,5%
-            //TODO: check algoritm
-            double fillTemp = 0;
-            percent += 100;
-            percent /= 2;
-            fillTemp = (percent * fillDelta) / 100; //fillTemp - how many ms i need add to fillMin?
-            fillTemp += percent; //fillTemp - was in ms, now add fillMin [ms]
-            fillTemp = (percent * 100) / 20; //change to Percent of fill (between 1.5% to 11.5%)
-            fillTemp /= 100;
-            return fillTemp;
-        }
+                //percentPowerToFill(wheelFill);
+        //private double percentPowerToFill(double percent)
+        //{
+        //    // -100% mean 0.8ms mean 4%
+        //    //    0% mean 1.5ms
+        //    //  100% mean 2.3ms mean 11,5%
+        //    //TODO: check algoritm
+        //    double fillTemp = 0;
+        //    percent += 100;
+        //    percent /= 2;
+        //    fillTemp = (percent * fillDelta) / 100; //fillTemp - how many ms i need add to fillMin?
+        //    fillTemp += percent; //fillTemp - was in ms, now add fillMin [ms]
+        //    fillTemp = (percent * 100) / 20; //change to Percent of fill (between 1.5% to 11.5%)
+        //    fillTemp /= 100;
+        //    return fillTemp;
+        //}
 
     }
 }
