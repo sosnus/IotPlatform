38044435d70fc5a326d23942054ac7a70aa751de
sosnus <sosnus15@gmail.com>
2017-05-09 19:32:16 +0200

    New proj - SosnusPlatform

---
+++ SosnusIotPlatform/SosnusIotLib/Pwm/Wheels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SosnusIotLib.Pwm
+{
+    class Wheels
+    {
+        //PwmServo360 _left;
+        //PwmServo360 _right;
+
+        //Wheels(PwmServo360 left, PwmServo360 right)
+        //{
+
+        //}
+
+        //void SetSpeed(int leftSpeed, int rightSpeed)
+        //{
+        //}
+    }
+}
