14425fe4a1c3504bac8f26130fb7bf17108da92a
sosnus <sosnus15@gmail.com>
2017-05-09 20:45:45 +0200

    SosnusPlatform.csproj edit

--- SosnusIotPlatform/SosnusPlatform/MainPage.xaml.cs
+++ SosnusIotPlatform/SosnusPlatform/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using SosnusIotLib.Pwm;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,6 +23,8 @@ namespace SosnusPlatform
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        PwmServo360 wheelLeft = new PwmServo360();
+        PwmServo360 wheelRight = new PwmServo360();
 
 
 
@@ -29,12 +32,21 @@ namespace SosnusPlatform
         {
             this.InitializeComponent();
             InitHardware();
+            GridMain.PointerWheelChanged += new PointerEventHandler(Pointer_Wheel_Changed);
+
+        }
+
+        void Pointer_Wheel_Changed(object sender, PointerRoutedEventArgs e)
+        {
+            ScrollCnt += pointerProperties.MouseWheelDelta / 120;
+            scrollTbl.Text = $" ScrollCnt = {ScrollCnt}";
         }
 
         private void InitHardware()
         {
-
-            throw new NotImplementedException();
+            wheelLeft.SetupServo360(23);
+            wheelLeft.SetupServo360(24);
+            //throw new NotImplementedException();
         }
     }
 }
