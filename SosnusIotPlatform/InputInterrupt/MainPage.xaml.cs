25fcc2fb1cbe6106779359acdce70a8e381694c4
sosnus <sosnus15@gmail.com>
2017-05-14 12:34:13 +0200

    Simplified project InputInterrupt.csproj

--- SosnusIotPlatform/InputInterrupt/MainPage.xaml.cs
+++ SosnusIotPlatform/InputInterrupt/MainPage.xaml.cs
@@ -1,26 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Gpio;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.Devices.Gpio;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace InputInterrupt
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private const int numer_pinu = 22; //number of pin where we plug switch
@@ -46,26 +28,22 @@ namespace InputInterrupt
             var t = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
             {
                 if (args.Edge == GpioPinEdge.FallingEdge)
-                {
                     tblInfo.Text = $"LOW state on pin {numer_pinu}";
-                }
                 else
-                {
                     tblInfo.Text = $"HIGH state on pin {numer_pinu}";
-                }
             });
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (mySwitch.Read() == GpioPinValue.Low)
-            {
-                tblInfo.Text = $"LOW state on pin {numer_pinu}";
-            }
-            else
-            {
-                tblInfo.Text = $"HIGH state on pin {numer_pinu}";
-            }
-        }
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (mySwitch.Read() == GpioPinValue.Low)
+        //    {
+        //        tblInfo.Text = $"LOW state on pin {numer_pinu}";
+        //    }
+        //    else
+        //    {
+        //        tblInfo.Text = $"HIGH state on pin {numer_pinu}";
+        //    }
+        //}
     }
 }
